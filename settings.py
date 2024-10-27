9159e5787fce7249fa1d7bd95b357ce77c07ce4f
R Sarveshvarn <151063233+gdev-kingx@users.noreply.github.com>
2024-10-27 13:57:13 +0530

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,18 @@
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+ROW_COUNT = 6
+COL_COUNT = 7
+
+PLAYER = 0
+AI = 1
+
+EMPTY = 0
+PLAYER_PIECE = 1
+AI_PIECE = 2
+
+SQSIZE = 100
+
+WIN_LENGTH = 4
